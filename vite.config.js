17a138e263c3d206579fb7b4034f3dd965bb70ee
mathysjlld <mathys.julliand@icloud.com>
2026-03-12 15:49:38 +0100

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: './', // Permet au site de fonctionner dans n'importe quel sous-dossier (comme sur WAMP)
-})
